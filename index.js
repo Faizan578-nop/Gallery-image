5f6ea12cfda911f868b439755fbff51db16ea7a2
Faizan Aslam Ridho <faizanaslamridho@gmail.com>
2024-12-13 07:00:21 +0700

    menambahkan animas

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ function rubahMiniKeBig (element){
     const srcMiniImage = miniImage.getAttribute("src")
     const srcBigImage = bigImage.getAttribute("src")
 
+    //fungsi untuk merubah src di image
     const changeSRC = function(){
         bigImage.removeAttribute("src")
-        bigImage.setAttribute("src",`${srcMiniImage}`)
+        bigImage.setAttribute("src",`${srcMiniImage}`)    
+        bigImage.classList.add("fade")
+
+
+        //Animation
+        if (srcMiniImage !== srcBigImage){setTimeout(function(){bigImage.classList.remove("fade")},100)}
+        else if (srcBigImage==srcMiniImage){bigImage.classList.remove("fade")}
+        //Animation End
+        
     }
-
-
     if (element.classList.contains = "mini-image"){
         changeSRC()
     }
@@ -24,9 +31,12 @@ function rubahMiniKeBig (element){
 
 const containerMiniElement = document.querySelector(".content-mini")
 
-
 containerMiniElement.addEventListener("click",function(e){
     rubahMiniKeBig(e.target)
-    // if (e.target.classList.contains("pa")){console.info("yes")}
-    // else {console.info("no")}
+
+    //Animation
+    const miniImage =Array.from( document.getElementsByClassName("mini-image"))
+    miniImage.forEach(function(value,index) {value.classList.remove("active")})
+    e.target.classList.add("active")
+    //Animation End
 })
